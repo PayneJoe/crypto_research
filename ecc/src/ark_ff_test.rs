e2c7da6b9e83f964eb04cb1fd4cd442fb711d2ba
PayneJoe <joepayne@163.com>
2023-12-20 23:41:18 +0800

    up

--- ecc/src/ark_ff_test.rs
+++ ecc/src/ark_ff_test.rs
@@ -1,3 +1,4 @@
+use ark_crypto_primitives::sponge::poseidon::{PoseidonDefaultConfig, PoseidonDefaultConfigEntry};
 use ark_ff::fields::{Field, Fp64, MontBackend, MontConfig};
 use num_bigint::*;
 
@@ -6,6 +7,27 @@ use num_bigint::*;
 #[generator = "3"]
 pub struct FqConfig;
 pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
+
+// impl PoseidonDefaultConfig<4> for MontBackend<FqConfig, 1> {
+//     const PARAMS_OPT_FOR_CONSTRAINTS: [PoseidonDefaultConfigEntry; 7] = [
+//         PoseidonDefaultConfigEntry::new(2, 17, 8, 31, 0),
+//         PoseidonDefaultConfigEntry::new(3, 5, 8, 56, 0),
+//         PoseidonDefaultConfigEntry::new(4, 5, 8, 56, 0),
+//         PoseidonDefaultConfigEntry::new(5, 5, 8, 57, 0),
+//         PoseidonDefaultConfigEntry::new(6, 5, 8, 57, 0),
+//         PoseidonDefaultConfigEntry::new(7, 5, 8, 57, 0),
+//         PoseidonDefaultConfigEntry::new(8, 5, 8, 57, 0),
+//     ];
+//     const PARAMS_OPT_FOR_WEIGHTS: [PoseidonDefaultConfigEntry; 7] = [
+//         PoseidonDefaultConfigEntry::new(2, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(3, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(4, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(5, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(6, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(7, 257, 8, 13, 0),
+//         PoseidonDefaultConfigEntry::new(8, 257, 8, 13, 0),
+//     ];
+// }
 
 #[cfg(test)]
 mod tests {
--- ecc/src/lib.rs
+++ ecc/src/lib.rs
@@ -1,8 +1,1 @@
 pub mod ark_ff_test;
-pub mod bn256;
-pub mod error;
-pub mod group;
-pub mod group_membership_check;
-pub mod grumpkin;
-pub mod keccak;
-pub mod utils;
