f36b562a3dbe8aca8964387c5e7d279297492e6e
PayneJoe <joepayne@163.com>
2024-01-22 19:38:33 +0800

    ops on polynomial done

--- pcs/src/polynomial/sparse.rs
+++ pcs/src/polynomial/sparse.rs
@@ -1,8 +1,9 @@
 use ecc::finite_field_arithmetic::traits::{BigInt, Field, PrimeField};
+use std::collections::BTreeMap;
 use std::ops::{Add, Mul};
 use std::str::FromStr;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SparsePolynomial<F: Field<2>> {
     coefficients: Vec<(usize, F)>,
 }
@@ -28,19 +29,98 @@ impl<F: Field<2>> SparsePolynomial<F> {
             .map(|(idx, coeff)| *coeff * x.pow(BigInt::from_str(idx.to_string().as_str()).unwrap()))
             .sum()
     }
+
+    fn is_zero(&self) -> bool {
+        self.coefficients.len() == 0
+    }
+
+    fn ZERO() -> Self {
+        Self {
+            coefficients: Vec::new(),
+        }
+    }
+
+    fn degreee(&self) -> usize {
+        if self.is_zero() {
+            return 0;
+        }
+        self.coefficients.last().unwrap().0
+    }
 }
 
 impl<F: Field<2>> Add for SparsePolynomial<F> {
     type Output = SparsePolynomial<F>;
     fn add(self, other: SparsePolynomial<F>) -> Self::Output {
-        unimplemented!()
+        &self + &other
     }
 }
 
 impl<F: Field<2>> Mul for SparsePolynomial<F> {
     type Output = SparsePolynomial<F>;
     fn mul(self, other: SparsePolynomial<F>) -> Self::Output {
-        unimplemented!()
+        &self * &other
+    }
+}
+
+// merge two polynomials into one
+impl<'a, 'b, F: Field<2>> Add<&'a SparsePolynomial<F>> for &'b SparsePolynomial<F> {
+    type Output = SparsePolynomial<F>;
+    fn add(self, other: &'a SparsePolynomial<F>) -> Self::Output {
+        if self.is_zero() {
+            return other.clone();
+        }
+        if other.is_zero() {
+            return self.clone();
+        }
+        let (mut pos_a, mut pos_b) = (0, 0);
+        let mut result: Vec<(usize, F)> = Vec::new();
+        loop {
+            if pos_a == self.coefficients.len() {
+                for i in pos_b..other.coefficients.len() {
+                    result.push(other.coefficients[i])
+                }
+                break;
+            }
+            if pos_b == other.coefficients.len() {
+                for i in pos_a..self.coefficients.len() {
+                    result.push(self.coefficients[i])
+                }
+                break;
+            }
+            let (idx_a, coeff_a) = self.coefficients[pos_a];
+            let (idx_b, coeff_b) = other.coefficients[pos_b];
+            if idx_a == idx_b {
+                result.push((idx_a, coeff_a + coeff_b));
+                (pos_a, pos_b) = (pos_a + 1, pos_b + 1);
+            } else if idx_a > idx_b {
+                result.push((idx_b, coeff_b));
+                pos_b = pos_b + 1;
+            } else if idx_a < idx_b {
+                result.push((idx_a, coeff_a));
+                pos_a = pos_a + 1;
+            }
+        }
+        SparsePolynomial::<F>::from_sparse_vec(result)
+    }
+}
+
+// naive implementation of polynomial multiplication
+impl<'a, 'b, F: Field<2>> Mul<&'a SparsePolynomial<F>> for &'b SparsePolynomial<F> {
+    type Output = SparsePolynomial<F>;
+    fn mul(self, other: &'a SparsePolynomial<F>) -> Self::Output {
+        if self.is_zero() || other.is_zero() {
+            return SparsePolynomial::<F>::ZERO();
+        }
+        let mut result: BTreeMap<usize, F> = BTreeMap::new();
+        for i in 0..self.coefficients.len() {
+            for j in 0..other.coefficients.len() {
+                let mut ent = result
+                    .entry(self.coefficients[i].0 + other.coefficients[j].0)
+                    .or_insert(F::ZERO());
+                *ent = *ent + self.coefficients[i].1 * other.coefficients[j].1;
+            }
+        }
+        SparsePolynomial::from_sparse_vec(result.into_iter().collect::<Vec<(usize, F)>>())
     }
 }
 
@@ -50,7 +130,10 @@ mod tests {
 
     #[test]
     fn test_evaluation() {
-        let sparse_vals = [(1, 10), (3, 20), (5, 30)]
+        let poly_params = [(1, 10), (3, 20), (5, 30)];
+        let (x, y) = (3, 7860);
+
+        let sparse_vals = poly_params
             .map(|(idx, v)| {
                 (
                     idx as usize,
@@ -58,9 +141,97 @@ mod tests {
                 )
             })
             .to_vec();
-        let x = PrimeField::from_str("3").unwrap();
-        let y = PrimeField::from_str("7860").unwrap();
+        let point = PrimeField::from_str(x.to_string().as_str()).unwrap();
+        let eval = PrimeField::from_str(y.to_string().as_str()).unwrap();
         let poly = SparsePolynomial::from_sparse_vec(sparse_vals);
-        assert_eq!(poly.evaluation(x), y);
+        assert_eq!(poly.evaluation(point), eval);
+    }
+
+    #[test]
+    fn test_add() {
+        let (params_a, params_b, params_c) = (
+            [(1, 10), (3, 20), (5, 30)],
+            [(2, 10), (3, 20), (6, 30)],
+            [(1, 10), (2, 10), (3, 40), (5, 30), (6, 30)],
+        );
+        let poly_a = SparsePolynomial::from_sparse_vec(
+            params_a
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        let poly_b = SparsePolynomial::from_sparse_vec(
+            params_b
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        let poly_c = SparsePolynomial::from_sparse_vec(
+            params_c
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        assert_eq!(poly_a + poly_b, poly_c);
+    }
+    #[test]
+    fn test_mul() {
+        let (params_a, params_b, params_c) = (
+            [(1, 10), (3, 20), (5, 30)],
+            [(2, 10), (3, 20), (6, 30)],
+            [
+                (3, 100),
+                (4, 200),
+                (7, 600),
+                (5, 200),
+                (6, 400),
+                (9, 600),
+                (8, 600),
+                (11, 900),
+            ],
+        );
+        let poly_a = SparsePolynomial::from_sparse_vec(
+            params_a
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        let poly_b = SparsePolynomial::from_sparse_vec(
+            params_b
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        let poly_c = SparsePolynomial::from_sparse_vec(
+            params_c
+                .map(|(idx, v)| {
+                    (
+                        idx as usize,
+                        PrimeField::from_str(v.to_string().as_str()).unwrap(),
+                    )
+                })
+                .to_vec(),
+        );
+        assert_eq!(poly_a * poly_b, poly_c);
     }
 }
